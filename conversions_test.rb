ceb02cc61d00a90c41d61fcd4b19881b11b8d77c
Lluvia Frias <lluvi.frias@gmail.com>
2020-04-06 16:02:33 +0000

    Done.

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,6 +1,6 @@
 require_relative './conversions.rb'
 
-puts "Given 0, it should return 0.0"
+puts "ounces to grams, when given 0, returns 0.0"
 answer = ounces_to_grams(0)
 if answer == 0.0
   puts "Passed!"
@@ -8,10 +8,20 @@ else
   puts "Test failed: got #{answer} instead"
 end
 
-puts "Given 1, it should return 28.3495"
+puts "ounces to grams, when given 1, returns 28.3495"
 answer = ounces_to_grams(1)
 if answer == 28.3495
   puts "Passed!"
 else
   puts "Test failed: got #{answer} instead"
 end
+
+
+
+#puts "Given 1, it should return 28.3495"
+#answer = ounces_to_grams(1)
+#if answer == 28.3495
+#  puts "Passed!"
+#else
+#  puts "Test failed: got #{answer} instead"
+#end
